e81184d8b5bd3302a46d0c5d4f2d1b14abfd6d63
L4mag <gheryesss@gmail.com>
2020-04-12 15:58:22 +0300

    Update storePost.js

--- contollers/storePost.js
+++ contollers/storePost.js
@@ -3,14 +3,8 @@ const path = require('path')
 
 module.exports = (req,res)=>{ 
     let image = req.files.image;  
-    image.mv(path.resolve(__dirname,'..','public/img',image.name), (error)=>{
-        if(error){
-            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
-            req.flash('validationErrors', validationErrors)
-            req.flash('data', req.body)
-            console.log
-        }
-        BlogPost.create({
+    image.mv(path.resolve(__dirname,'..','public/img',image.name), async (error)=>{
+        await BlogPost.create({
             ...req.body,
             image: '/img/' + image.name
         })
